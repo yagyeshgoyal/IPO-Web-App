88280f803d827c3f341a8070f31ff7901e8b2bab
yagyeshgoyal <yagyeshgoyal123@gmail.com>
2025-06-27 13:17:19 +0530

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- Admin/src/Components/AllBrokers.jsx
+++ Admin/src/Components/AllBrokers.jsx
@@ -8,24 +8,24 @@ function AllBrokers(){
         <div>
             <p className='text-[30px] font-bold text-[#7059EBE5] px-4'>Made to<span className='text-[#2AB479]'>Trade</span></p>
             <Broker name="Upstox" color="rgba(131, 162, 255, 0.302)" image={assest.upstox}/>
-            <Broker name="AngleOne" color="#FFD28F66" image={assest.upstox}/>
-            <Broker name="Groww" color="#83A2FF4D" image={assest.upstox}/>
-            <Broker name="Dhan" color="#FFD28F66" image={assest.upstox}/>
-            <Broker name="Alice Blue" color="#83A2FF4D" image={assest.upstox}/>
-            <Broker name="Axis Direct" color="#FFD28F66" image={assest.upstox}/>
-            <Broker name="Fyers" color="#83A2FF4D" image={assest.upstox}/>
-            <Broker name="Geojit" color="#FFD28F66" image={assest.upstox}/>
-            <Broker name="HDFC Securities" color="#83A2FF4D" image={assest.upstox}/>
-            <Broker name="IIFL securities" color="#FFD28F66" image={assest.upstox}/>
-            <Broker name="Kotak securities" color="#83A2FF4D" image={assest.upstox}/>
-            <Broker name="Motilal Oswal" color="#FFD28F66" image={assest.upstox}/>
-            <Broker name="Nuvama" color="#83A2FF4D" image={assest.upstox}/>
-            <Broker name="Paytm Money" color="#FFD28F66" image={assest.upstox}/>
-            <Broker name="SBI securities" color="#83A2FF4D" image={assest.upstox}/>
-            <Broker name="Sharekhan" color="#FFD28F66" image={assest.upstox}/>
-            <Broker name="SMC Global" color="#83A2FF4D" image={assest.upstox}/>
-            <Broker name="5Paisa" color="#FFD28F66" image={assest.upstox}/>
-            <Broker name="Zerodha" color="#83A2FF4D" image={assest.upstox}/>
+            <Broker name="AngleOne" color="#FFD28F66" image={assest.Angleone}/>
+            <Broker name="Groww" color="#83A2FF4D" image={assest.Groww}/>
+            <Broker name="Dhan" color="#FFD28F66" image={assest.Dhan}/>
+            <Broker name="Alice Blue" color="#83A2FF4D" image={assest.Aliceblue}/>
+            <Broker name="Axis Direct" color="#FFD28F66" image={assest.Axisdirect}/>
+            <Broker name="Fyers" color="#83A2FF4D" image={assest.Fyers}/>
+            <Broker name="Geojit" color="#FFD28F66" image={assest.Geojit}/>
+            <Broker name="HDFC Securities" color="#83A2FF4D" image={assest.HDFC}/>
+            <Broker name="IIFL securities" color="#FFD28F66" image={assest.IIFL}/>
+            <Broker name="Kotak securities" color="#83A2FF4D" image={assest.Kotak}/>
+            <Broker name="Motilal Oswal" color="#FFD28F66" image={assest.Motilal}/>
+            <Broker name="Nuvama" color="#83A2FF4D" image={assest.Nuvama}/>
+            <Broker name="Paytm Money" color="#FFD28F66" image={assest.Paytm}/>
+            <Broker name="SBI securities" color="#83A2FF4D" image={assest.SBI}/>
+            <Broker name="Sharekhan" color="#FFD28F66" image={assest.Sharekhan}/>
+            <Broker name="SMC Global" color="#83A2FF4D" image={assest.SMC}/>
+            <Broker name="5Paisa" color="#FFD28F66" image={assest.Paisa}/>
+            <Broker name="Zerodha" color="#83A2FF4D" image={assest.Zerodha}/>
             
             
         </div>
--- Admin/src/assets/assets.js
+++ Admin/src/assets/assets.js
@@ -1,10 +1,47 @@
 import android from './android app icon.png';
 import rakesh from './Rakesh_Jhunjhunwala.png';
 import upstox from './upstox.png';
+import Paisa from './5Paisa.png';
+import Aliceblue from './Alice_Blue.png';
+import Angleone from './AngleOne.png';
+import Axisdirect from './Axis_Direct.png';
+import Dhan from './Dhan.jpg';
+import Fyers from './Fyers.png';
+import Geojit from './Geojit.png';
+import Groww from './Groww.png';
+import HDFC from './HDFC_Securities.png';
+import IIFL from './IIFL_securities.png';
+import Kotak from './Kotak_securities.png';
+import Motilal from './Motilal_Oswal.jpg';
+import Nuvama from './Nuvama.png';
+import Paytm from './Paytm_Money.png';
+import SBI from './SBI_securities.png';
+import Sharekhan from './Sharekhan.png';
+import SMC from './SMC_Global.png';
+import Zerodha from './Zerodha.png';
 
 
 export const assest = {
     android,
     rakesh,
-    upstox
+    upstox,
+    Paisa,
+    Aliceblue,
+    Angleone,
+    Axisdirect,
+    Dhan,
+    Fyers,
+    Geojit,
+    Groww,
+    HDFC,
+    IIFL,
+    Kotak,
+    Motilal,
+    Nuvama,
+    Paytm,
+    SBI,
+    Sharekhan,
+    SMC,
+    Zerodha
+    
 };
